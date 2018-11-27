6a50d5a0f9ae94b9f9d5322785b318d23dec39b3
ronnieoginga <oginga001ronney@gmail.com>
2018-11-27 12:13:06 +0300

    added js link

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var number1 = prompt("Enter a number:");
+var number2 = prompt("Enter another number:");
+alert(add(number1, number2));
